138fb9bffbea1fd67aa3da17ece5a0dbe7ce8ba9
suleman-721 <suleman.721@outlook.com>
2017-04-03 10:22:12 +0500

    Assignment

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+// ****************Table of 4*******************
+ for(var i=1; i<=10; i++)
+{
+    document.write("<br/>4 x " +i +"= " +(i*4));
+}
+
+
+// Right Angled Triangle
+for(var j=0; j<=8; j++)
+{
+ for(var k=0;k<=j; k++){
+  document.write("*");
+}
+document.write(" <br/>");
+}
+
